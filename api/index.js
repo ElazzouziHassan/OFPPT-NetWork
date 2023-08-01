f3fd3eab5f1207f882ecfef2c92d2713a549acf1
wizardy <wizardhassan99@gmail.com>
2023-08-01 19:39:26 +0100

    updates : endpoints

--- api/index.js
+++ api/index.js
@@ -1,10 +1,27 @@
 import express from "express";
+
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import likeRoutes from "./routes/likes.js";
+import commentRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
+
 import dotenv from 'dotenv'
 
+
 dotenv.config()
+
 const app = express(); const PORT = process.env.HTTP_SERVER_PORT;
 
 
+
+
+app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/likes", likeRoutes)
+app.use("/api/comments", commentRoutes)
+
 app.listen(PORT, () => {
-  console.log("API working!");
-});
+  console.log(`Server running on:  http://localhost:${PORT}/`)
+})

3d7fcbfff5de7e5c6d0af911eb821aed81db0aec
wizardy <wizardhassan99@gmail.com>
2023-08-01 19:18:57 +0100

    port added to env file

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import dotenv from 'dotenv'
 
-const app = express();
+dotenv.config()
+const app = express(); const PORT = process.env.HTTP_SERVER_PORT;
 
-app.listen(8800, () => {
+
+app.listen(PORT, () => {
   console.log("API working!");
 });

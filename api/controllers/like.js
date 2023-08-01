a0fc4d0c74930e6e97d6eae100b8a02f15d1bed1
wizardy <wizardhassan99@gmail.com>
2023-08-01 20:06:24 +0100

    like controller

---
+++ api/controllers/like.js
@@ -0,0 +1,11 @@
+export const getLikes = (req, res) => {
+  // to do operation 
+}
+
+export const addLike = (req, res) => {
+  // to do operation 
+}
+
+export const removeLike = (req, res) => {
+  // to do operation 
+}

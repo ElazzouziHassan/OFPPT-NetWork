ab5c33ac16a4cc0117ad3aed26349219661ca3c3
wizardy <wizardhassan99@gmail.com>
2023-08-01 20:03:22 +0100

    authenticatin updated

---
+++ api/controllers/auth.js
@@ -0,0 +1,11 @@
+export const register = (req, res) => {
+  // to do operation
+}
+
+export const login = (req, res) => {
+  // to do operation
+}
+
+export const logout = (req, res) => {
+  // to do operation
+}

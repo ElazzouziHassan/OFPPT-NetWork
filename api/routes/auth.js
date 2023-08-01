0c1c59addfe79aa8174e30e9c95a0e7b91356152
wizardy <wizardhassan99@gmail.com>
2023-08-01 20:04:09 +0100

    new updates on auth

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,10 @@
 import express from "express"
+import { login, logout, register } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get("/auth", (req, res) => {
-  res.send('this route is for authentication!')
-});
+router.get("/login", login);
+router.get("/register", register);
+router.get("/logout", logout);
 
 export default router

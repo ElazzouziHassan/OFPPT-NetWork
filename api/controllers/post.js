f80391833458316f79ca2bd657e96e1906821ea0
wizardy <wizardhassan99@gmail.com>
2023-08-01 20:06:33 +0100

    post controller

---
+++ api/controllers/post.js
@@ -0,0 +1,15 @@
+export const getPosts = (req, res) => {
+  // to do operation 
+}
+
+export const addPost = (req, res) => {
+  // to do operation 
+}
+
+export const deletePost = (req, res) => {
+  // to do operation 
+}
+
+export const updatePost = (req, res) => {
+  // to do operation 
+}

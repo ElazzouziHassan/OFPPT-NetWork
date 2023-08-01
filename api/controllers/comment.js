73418c1ecf688e324b3a675bfebfbd149daa1678
wizardy <wizardhassan99@gmail.com>
2023-08-01 20:06:16 +0100

    comment controller

---
+++ api/controllers/comment.js
@@ -0,0 +1,15 @@
+export const getComments = (req, res) => {
+  // to do operation 
+}
+
+export const addComment = (req, res) => {
+  // to do operation 
+}
+
+export const deleteComment = (req, res) => {
+  // to do operation 
+}
+
+export const updateComment = (req, res) => {
+  // to do operation 
+}

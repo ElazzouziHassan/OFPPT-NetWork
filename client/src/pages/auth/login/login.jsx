f3b2e1d9841f11fb9886401fafee85463118a684
wizardy <wizardhassan99@gmail.com>
2023-08-03 21:19:10 +0100

    login page

--- client/src/pages/auth/login/login.jsx
+++ client/src/pages/auth/login/login.jsx
@@ -1,8 +1,26 @@
+import "./login.scss"
 import React from 'react'
+
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Welcome to</h1>
+          <p><strong>OFPPT NetWork</strong>, The space where you can share, partipate and collaborate with your collegues</p>
+          <span>Accounts distributed by your school</span>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form action="#">
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 

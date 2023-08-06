09225808598d0e3c5fb0b0bce6acd41c22b19996
wizardy <wizardhassan99@gmail.com>
2023-08-06 20:35:55 +0100

    new updates 8.6.2023

--- client/src/pages/auth/login/login.jsx
+++ client/src/pages/auth/login/login.jsx
@@ -1,27 +1,37 @@
-import "./login.scss"
-import React from 'react'
+import "./login.scss";
 
-
-function Login() {
-  return (
-    <div className="login">
-      <div className="card">
-        <div className="left">
-          <h1>Welcome to</h1>
-          <p><strong>OFPPT NetWork</strong>, The space where you can share, partipate and collaborate with your collegues</p>
-          <span>Accounts distributed by your school</span>
+const Login = () => {
+    
+    return (
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <img src="src/assets/ofppt.png" alt="ofppt" />
+                    <h3>Welcome to OFPPT NetWork</h3>
+                    <div className="new-users">
+                        <span>try to login with your school account</span><br />
+                    </div>
+                </div>
+                <div className="right">
+                    <h3>login</h3>
+                    <form>
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder='Username'
+                        />
+                        <span></span>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder='Password'
+                        />
+                        <span></span>
+                        <button >Login</button>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div className="right">
-          <h1>Login</h1>
-          <form action="#">
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <button>Login</button>
-          </form>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
-
 export default Login

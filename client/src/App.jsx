a06524c3d8fd000765706df3ae00004ff39c3a7f
wizardy <wizardhassan99@gmail.com>
2023-08-10 22:13:52 +0100

    routes set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,27 @@
-
 import './App.css'
 import Login from './pages/auth/login/login'
+import Home from './pages/home/home'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/",
+    element: <Home/>
+  }
+]);
 
 function App() {
 
   return (
     <div className="App">
-      <Login/>
+      <RouterProvider router={router} />
     </div>
   )
 }

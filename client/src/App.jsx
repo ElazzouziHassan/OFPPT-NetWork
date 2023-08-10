d0b05c6fe80c50329b4dec9f11f0e40c87619296
wizardy <wizardhassan99@gmail.com>
2023-08-10 22:26:02 +0100

    outlet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css'
 import Login from './pages/auth/login/login'
 import Home from './pages/home/home'
+import NavBar from './components/nav-bar/NavBar'
+import LeftBar from './components/nav-bar/LeftBar'
+import RightBar from './components/nav-bar/RighBar'
 import {
   createBrowserRouter,
+  Outlet,
   RouterProvider,
 } from "react-router-dom";
+
+const layout = () => {
+  return (
+    <div>
+      <NavBar />
+      <div style={{ dispaly: "flex" }}>
+        <LeftBar />
+        <Outlet />
+        <RightBar/>
+      </div>
+    </div>
+  )
+}
 
 const router = createBrowserRouter([
   {

e5cabdb1697f31ef11ad7e969b17d5f666ab4ecc
wizardy <wizardhassan99@gmail.com>
2023-08-11 22:24:10 +0100

    new changes on nav-bar

--- client/src/components/nav-bar/NavBar.jsx
+++ client/src/components/nav-bar/NavBar.jsx
@@ -1,11 +1,26 @@
 import "./navbar.scss";
-
+import OtherHousesOutlinedIcon from '@mui/icons-material/OtherHousesOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import BookmarkOutlinedIcon from '@mui/icons-material/BookmarkOutlined';
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className="nav-bar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration:"none" }}>
+          <span>OFPPT NetWork</span>
+        </Link>
+        
+      </div>
+      <div className="right"></div>
+    </div>
   )
 }
 

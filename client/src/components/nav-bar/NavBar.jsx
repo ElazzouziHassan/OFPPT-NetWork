3fbaddc9745e72ea858db1437d44dd3a61c9774c
wizardy <wizardhassan99@gmail.com>
2023-08-11 22:32:30 +0100

    nav bar updates 8/11/2023

--- client/src/components/nav-bar/NavBar.jsx
+++ client/src/components/nav-bar/NavBar.jsx
@@ -5,7 +5,7 @@ import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import BookmarkOutlinedIcon from '@mui/icons-material/BookmarkOutlined';
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
 
 import React from 'react'
 import { Link } from 'react-router-dom'
@@ -17,9 +17,21 @@ function NavBar() {
         <Link to="/" style={{ textDecoration:"none" }}>
           <span>OFPPT NetWork</span>
         </Link>
-        
+        <OtherHousesOutlinedIcon />
+        <PeopleAltOutlinedIcon />
+        <div className="serch">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search.." />
+        </div> 
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <NotificationsNoneOutlinedIcon/>
+        <EmailOutlinedIcon/>
+        <BookmarkBorderOutlinedIcon />
+        <div className="user">
+          <img src="#" alt="user" />
+        </div>
+      </div>
     </div>
   )
 }

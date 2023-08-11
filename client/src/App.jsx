db8c9ecb8fe14759921518bb3a456123ca89c695
wizardy <wizardhassan99@gmail.com>
2023-08-11 21:54:34 +0100

    new updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -43,7 +43,11 @@ function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <ProtectedRoute><Layout /></ProtectedRoute>,
+      element:
+        <ProtectedRoute>
+        <Layout />
+        </ProtectedRoute>
+      ,
       children: [
         {
           path: '/',

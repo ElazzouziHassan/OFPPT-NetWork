1483178299c7cb687c69f19b319df5493c9e506b
wizardy <wizardhassan99@gmail.com>
2023-08-12 12:59:26 +0100

    new updates on left bar

--- client/src/components/left-bar/LeftBar.jsx
+++ client/src/components/left-bar/LeftBar.jsx
@@ -4,7 +4,50 @@ import React from 'react'
 
 function LeftBar() {
   return (
-    <div>LeftBar</div>
+    <div className="left-bar">
+      <div className="container">
+        <div className="menu">
+          <div className="user">
+            <img src="#" alt="user" />
+            <span>User</span>
+          </div>
+          <div className="item">
+            <img src="#" alt="Home" />
+            <span>Home</span>
+          </div>
+          <div className="item">
+            <img src="#" alt="Friends" />
+            <span>Friends</span>
+          </div>
+          <div className="item">
+            <img src="#" alt="Messages" />
+            <span>Messages</span>
+          </div>
+        </div>
+        <hr />
+        <span>others</span>
+        <div className="others">
+          <div className="item">
+            <img src="#" alt="adverts" />
+            <span>adverts</span>
+          </div>
+          <div className="item">
+            <img src="#" alt="favorites" />
+            <span>favorites</span>
+          </div>
+          <div className="item">
+            <img src="#" alt="Messages" />
+            <span>Messages</span>
+          </div>
+        </div>
+        <hr />
+        <div className="footer">
+          <span>OFPPT NETWORK @2023</span>
+          <p>ALL RIGHTS ARE RESERVED.</p>
+        </div>
+
+      </div>
+    </div>
   )
 }
 

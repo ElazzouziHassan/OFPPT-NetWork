94e94ea919fe1da5fdcfb4325fed79717f8b9aac
wizardy <wizardhassan99@gmail.com>
2023-08-17 19:05:02 +0100

    new updates on right bar : suggestions section

--- client/src/components/right-bar/RightBar.jsx
+++ client/src/components/right-bar/RightBar.jsx
@@ -1,11 +1,47 @@
 import "./rightbar.scss";
-
 import React from 'react'
+
+import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
+import PersonRemoveOutlinedIcon from '@mui/icons-material/PersonRemoveOutlined';
 
 function RightBar() {
   return (
     <div className="right-bar">
-      <h1>right bar right bar right bar</h1>
+      <div className="container">
+        <div className="item">
+          <span>Suggestions for you</span>
+          <div className="user">
+            <div className="user-info">
+              <img src="#" alt="user" />
+              <span>Jane Doe</span>
+            </div>
+            <div className="actions">
+              <button><PersonAddAltOutlinedIcon className="icon"/></button>
+              <button><PersonRemoveOutlinedIcon className="icon"/></button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img src="#" alt="user" />
+              <span>Jane Doe</span>
+            </div>
+            <div className="actions">
+              <button><PersonAddAltOutlinedIcon className="icon"/></button>
+              <button><PersonRemoveOutlinedIcon className="icon"/></button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img src="#" alt="user" />
+              <span>Jane Doe</span>
+            </div>
+            <div className="actions">
+              <button><PersonAddAltOutlinedIcon className="icon"/></button>
+              <button><PersonRemoveOutlinedIcon className="icon"/></button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

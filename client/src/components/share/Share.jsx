aa8d7ac33841a77aac8acd40b4f4e75390160dd9
wizardy <wizardhassan99@gmail.com>
2023-08-18 20:16:29 +0100

    commit

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,10 +1,29 @@
 import "./share.scss";
 
 import React from 'react'
+import AlternateEmailOutlinedIcon from '@mui/icons-material/AlternateEmailOutlined';
+import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 
 function Share() {
   return (
-    <div>Share</div>
+    <div className="share-post">
+      <div className="user">
+        <img src="#" alt="user" />
+      </div>
+      <div className="shared-content">
+        <input type="text" placeholder="What's on your mind !"/>
+      </div>
+      <hr />
+      <div className="actions">
+        <div className="left">
+          <button><FileUploadOutlinedIcon/></button>
+          <button><AlternateEmailOutlinedIcon/></button>
+        </div>
+        <div className="right">
+          <button>post</button>
+        </div>
+      </div>
+    </div>
   )
 }
 

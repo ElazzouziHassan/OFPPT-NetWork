4ab0a189c550b1a86e0d84c1ab46ee48f080a4ad
wizardy <wizardhassan99@gmail.com>
2023-08-18 20:35:20 +0100

    commit

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,4 +1,5 @@
 import "./share.scss";
+import User from '../../assets/user.gif'
 
 import React from 'react'
 import AlternateEmailOutlinedIcon from '@mui/icons-material/AlternateEmailOutlined';
@@ -7,22 +8,24 @@ import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 function Share() {
   return (
     <div className="share-post">
-      <div className="user">
-        <img src="#" alt="user" />
-      </div>
-      <div className="shared-content">
-        <input type="text" placeholder="What's on your mind !"/>
+      <div className="top">
+        <div className="user">
+          <img src={User} alt="user" />
+        </div>
+        <div className="shared-content">
+          <input type="text" placeholder="What's on your mind !"/>
+        </div>
       </div>
       <hr />
-      <div className="actions">
-        <div className="left">
-          <button><FileUploadOutlinedIcon/></button>
-          <button><AlternateEmailOutlinedIcon/></button>
+        <div className="actions">
+          <div className="left">
+            <button><FileUploadOutlinedIcon className="icon"/></button>
+            <button><AlternateEmailOutlinedIcon className="icon"/></button>
+          </div>
+          <div className="right">
+            <button><span>post</span></button>
+          </div>
         </div>
-        <div className="right">
-          <button>post</button>
-        </div>
-      </div>
     </div>
   )
 }

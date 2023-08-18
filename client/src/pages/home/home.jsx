54a845c492291bb1957a8aa3334d84828e74f586
wizardy <wizardhassan99@gmail.com>
2023-08-18 20:16:23 +0100

    commit

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Share from '../../components/share/Share'
+import Posts from '../../components/posts/Posts'
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <Share />
+      <Posts/>
     </div>
   )
 }

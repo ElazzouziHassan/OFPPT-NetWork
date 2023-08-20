d54f147f7796c0382016831a3ca7683e5a465f55
wizardy <wizardhassan99@gmail.com>
2023-08-20 20:04:27 +0100

    commit

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -8,7 +8,20 @@ import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 function Share() {
   return (
     <div className="share-post">
-      <h1>Share your post</h1>
+      <div className="new-post">
+        <img src="#" alt="user" />
+        <input type="text" placeholder="What's on your mind !" />
+      </div>
+      <hr />
+      <div className="actions">
+        <div className="left">
+          <button>add file</button>
+          <button>tag</button>
+        </div>
+        <div className="right">
+          <button>post</button>
+        </div>
+      </div>
     </div>
   )
 }

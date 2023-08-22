9ba8d878f6d7efee9fdb91c5c0e55401e806774a
wizardy <wizardhassan99@gmail.com>
2023-08-22 21:04:11 +0100

    commit

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,5 +1,7 @@
 import "./post.scss";
 import React from 'react'
+import User from "../../assets/user.gif"
+import Verify from '../../assets/icons/verify.png'
 
 // !just for test:
 import Image from '../../assets/mediaPost.jpg'
@@ -20,13 +22,15 @@ function Post() {
     <div className="post">
       <div className="top">
         <div className="user">
-          <img src="#" alt="user" />
+          <img src={User} alt="user" />
           <span>Jhon Doe</span>
+          <img src={Verify} className="verify" />
         </div>
         <div className="time">
           <p>1 Min Ago</p>
         </div>
       </div>
+      <hr />
       <div className="content">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         {

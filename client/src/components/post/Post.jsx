324d71f9194181ef66bc3458c102b69b752c5ff4
wizardy <wizardhassan99@gmail.com>
2023-08-23 19:31:15 +0100

    commit

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -32,7 +32,7 @@ function Post() {
       </div>
       <hr />
       <div className="content">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <p>React apps are made out of components. A component is a piece of the UI (user interface) that has its own logic and appearance. A component can be as small as a button, or as large as an entire page.</p>
         {
           media
           ?

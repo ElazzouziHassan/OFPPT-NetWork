f5112d53d9671f8b4b404b76d50c940b54c3380a
wizardy <wizardhassan99@gmail.com>
2023-08-24 12:55:53 +0100

    commit

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -2,13 +2,21 @@ import "./home.scss";
 import React from 'react'
 import Share from '../../components/share/Share'
 import Posts from '../../components/posts/Posts'
+import LeftBar from '../../components/left-bar/LeftBar'
+import RightBar from '../../components/right-bar/RightBar'
 
 
 function Home() {
   return (
     <div className="home">
-      <Share />
-      <Posts/>
+      <div style={{ display:"flex" }}>
+        <LeftBar />
+          <div className="feeds">
+            <Share />
+            <Posts/>
+          </div>
+          <RightBar/>
+        </div>
     </div>
   )
 }

a61d77e905c3ed960f514eb455d678f4bed59561
wizardy <ezhassan.info@gmail.com>
2024-02-17 22:32:50 +0100

    commit

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,8 +1,17 @@
+import { db } from "../connection";
+
 export const register = (req, res) => {
   
   // verify user existence !
 
   const q = "SELECT FROM users WHERE username = ?";
+
+  db.query(q, [req.body.username], (error, data) => {
+    
+    if (error) return res.status(500).json(error);
+    if (data.length) return res.status(409).json("user is already exists !");
+
+  })
 
 
 }
